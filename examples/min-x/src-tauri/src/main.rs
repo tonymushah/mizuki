208efd2897a55783ce376f291c5c867506fa81d1
tonymushah <tonyramanantsalama@gmail.com>
2023-12-16 18:10:14 +0300

    Resimplifed shit

--- examples/min-x/src-tauri/src/main.rs
+++ examples/min-x/src-tauri/src/main.rs
@@ -1,14 +1,48 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use mizuki::MizukiPluginTrait;
+use async_graphql::{EmptyMutation, Object, EmptySubscription, Schema};
+use tauri::{plugin::Plugin, Runtime};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+struct Query;
+
+#[Object]
+impl Query{
+    async fn value(&self) -> u32 {
+        100
+    }
+}
+
+struct MyPlug;
+impl<R> MizukiPluginTrait<R, Query, EmptyMutation, EmptySubscription> for MyPlug 
+where R : Runtime
+{
+    fn schema(&self) -> async_graphql::Schema<Query, EmptyMutation, EmptySubscription> {
+        Schema::new(Query, EmptyMutation, EmptySubscription)
+    }
+}
+
+impl<R> Plugin<R> for MyPlug 
+where R: Runtime
+{
+    fn name(&self) -> &'static str {
+        "my-plug"
+    }
+    fn extend_api(&mut self, invoke: tauri::Invoke<R>) {
+        <MyPlug as MizukiPluginTrait<R, Query, EmptyMutation, EmptySubscription>>::extend_api(self, invoke)
+    }
+}
+
 fn main() {
     tauri::Builder::default()
+        .plugin(MyPlug)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/traits.rs
+++ src/traits.rs
@@ -3,18 +3,16 @@ use std::{path::PathBuf, io::{BufWriter, Write}, fs::File};
 use crate::subscription::SubscriptionRequest;
 use async_graphql::{futures_util::StreamExt, BatchRequest, ObjectType, Schema, SubscriptionType};
 use tauri::{
-  plugin::Plugin, AppHandle, InvokeError, Manager, PageLoadPayload, RunEvent, Runtime, Window,
+  plugin::Plugin, InvokeError, Manager, Runtime,
 };
 
-pub trait MizukiPluginTrait<R, Query, Mutation, Subscription>: Send
+pub trait MizukiPluginTrait<R, Query, Mutation, Subscription>: Plugin<R>
 where
   Query: ObjectType + 'static,
   Mutation: ObjectType + 'static,
   Subscription: SubscriptionType + 'static,
   R: Runtime,
 {
-  // Your plugin name
-  fn name(&self) -> &'static str;
   fn schema(&self) -> Schema<Query, Mutation, Subscription>;
   fn sdl(&self) -> String {
     self.schema().sdl()
@@ -23,51 +21,6 @@ where
   fn export_sdl(&self, path: PathBuf) -> std::io::Result<()> {
     let mut file = BufWriter::new(File::create(path)?);
     file.write_all(self.sdl().as_bytes())
-  }
-  #[allow(unused_variables)]
-  fn initialize(
-    &mut self,
-    app: &AppHandle<R>,
-    config: serde_json::Value,
-  ) -> tauri::plugin::Result<()> {
-    Ok(())
-  }
-
-  /// Add the provided JavaScript to a list of scripts that should be run after the global object has been created,
-  /// but before the HTML document has been parsed and before any other script included by the HTML document is run.
-  ///
-  /// Since it runs on all top-level document and child frame page navigations,
-  /// it's recommended to check the `window.location` to guard your script from running on unexpected origins.
-  ///
-  /// The script is wrapped into its own context with `(function () { /* your script here */ })();`,
-  /// so global variables must be assigned to `window` instead of implicitly declared.
-  fn initialization_script(&self) -> Option<String> {
-    None
-  }
-
-  /// Callback invoked when the webview is created.
-  #[allow(unused_variables)]
-  fn created(&mut self, window: Window<R>) {}
-
-  /// Callback invoked when the webview performs a navigation to a page.
-  #[allow(unused_variables)]
-  fn on_page_load(&mut self, window: Window<R>, payload: PageLoadPayload) {}
-
-  /// Callback invoked when the event loop receives a new event.
-  #[allow(unused_variables)]
-  fn on_event(&mut self, app: &AppHandle<R>, event: &RunEvent) {}
-}
-
-impl<R, Query, Mutation, Subscription> Plugin<R>
-  for dyn MizukiPluginTrait<R, Query, Mutation, Subscription>
-where
-  R: Runtime,
-  Query: ObjectType + 'static,
-  Mutation: ObjectType + 'static,
-  Subscription: SubscriptionType + 'static,
-{
-  fn name(&self) -> &'static str {
-    <Self as MizukiPluginTrait<R, Query, Mutation, Subscription>>::name(self)
   }
   fn extend_api(&mut self, invoke: tauri::Invoke<R>) {
     let window = invoke.message.window();
@@ -110,26 +63,5 @@ where
         cmd
       )),
     }
-  }
-  fn created(&mut self, window: tauri::Window<R>) {
-    <Self as MizukiPluginTrait<R, Query, Mutation, Subscription>>::created(self, window);
-  }
-  fn initialization_script(&self) -> Option<String> {
-    <Self as MizukiPluginTrait<R, Query, Mutation, Subscription>>::initialization_script(self)
-  }
-  fn initialize(
-    &mut self,
-    app: &tauri::AppHandle<R>,
-    config: serde_json::Value,
-  ) -> tauri::plugin::Result<()> {
-    <Self as MizukiPluginTrait<R, Query, Mutation, Subscription>>::initialize(self, app, config)
-  }
-  fn on_event(&mut self, app: &tauri::AppHandle<R>, event: &tauri::RunEvent) {
-    <Self as MizukiPluginTrait<R, Query, Mutation, Subscription>>::on_event(self, app, event)
-  }
-  fn on_page_load(&mut self, window: tauri::Window<R>, payload: tauri::PageLoadPayload) {
-    <Self as MizukiPluginTrait<R, Query, Mutation, Subscription>>::on_page_load(
-      self, window, payload,
-    )
   }
 }
