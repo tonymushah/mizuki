100be8a9a481a38ae70d53c26c472fa24ab74cc9
tonymushah <tonyramanantsalama@gmail.com>
2025-01-07 11:06:29 +0300

    added grahql  test

---
+++ examples/svelte-apollo-app/codegen.ts
@@ -0,0 +1,15 @@
+import {CodegenConfig} from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  schema: './myschema.graphqls',
+  documents: ['src/**/*.ts', 'src/**/*.svelte'],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    './src/lib/gql/': {
+      preset: 'client',
+      plugins: []
+    }
+  }
+}
+
+export default config
--- examples/svelte-apollo-app/plugin/mizuki-test/src/lib.rs
+++ examples/svelte-apollo-app/plugin/mizuki-test/src/lib.rs
@@ -1,1 +1,40 @@
+use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema};
+use mizuki::{Builder, MizukiPlugin};
+use std::{
+  fs::File,
+  io::{BufWriter, Write},
+  path::Path,
+};
+use tauri::Runtime;
+
 pub struct Query;
+
+#[Object]
+impl Query {
+  async fn say(&self, name: Option<String>) -> String {
+    if let Some(name) = name {
+      format!("Hello {name}!")
+    } else {
+      "Insert your name please!".into()
+    }
+  }
+}
+
+pub fn init<R: Runtime>() -> MizukiPlugin<R, Query, EmptyMutation, EmptySubscription> {
+  Builder::new(
+    "mizuki-test",
+    Schema::new(Query, EmptyMutation, EmptySubscription),
+  )
+  .setup(|_app, _, schema| {
+    #[cfg(debug_assertions)]
+    {
+      let sdl = schema.sdl();
+      let sdl_file = File::create(Path::new("../myschema.graphqls"))?;
+      let mut buf_write = BufWriter::new(sdl_file);
+      buf_write.write_all(sdl.as_bytes())?;
+      buf_write.flush()?;
+    }
+    Ok(())
+  })
+  .build()
+}
