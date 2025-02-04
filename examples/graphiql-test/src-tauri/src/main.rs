7cfbe472fdf6a8d29c568ce72aeba12c50b40715
tonymushah <tonyramanantsalama@gmail.com>
2025-02-04 22:38:33 +0300

    more code changes

--- examples/graphiql-test/src-tauri/src/main.rs
+++ examples/graphiql-test/src-tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    graphiql_test_lib::run()
+  graphiql_test_lib::run()
 }
--- examples/svelte-apollo-app/src-tauri/src/main.rs
+++ examples/svelte-apollo-app/src-tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    svelte_apollo_app_lib::run()
+  svelte_apollo_app_lib::run()
 }
