0f503a7273eddca85e4b640fac27247d430d60f2
tonymushah <tonyramanantsalama@gmail.com>
2025-10-21 22:49:27 +0300

    fmt

--- examples/preact-app/src/gql/gql.ts
+++ examples/preact-app/src/gql/gql.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import * as types from './graphql';
-import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import * as types from './graphql'
+import type {TypedDocumentNode as DocumentNode} from '@graphql-typed-document-node/core'
 
 /**
  * Map of all GraphQL operations in the project.
@@ -13,12 +13,16 @@ import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-    "\n    subscription messages {\n        watchMessages\n    }\n": types.MessagesDocument,
-    "\n    mutation sendMessage($message: String!) {\n        sendMessage(message: $message)\n    }\n": types.SendMessageDocument,
-    "\nquery getHero {\n  hero {\n    name\n  }\n}\n": types.GetHeroDocument,
-    "\n  query notHero {\n    notHero {\n      name\n    }\n  }\n": types.NotHeroDocument,
-    "\n  subscription MessageSub {\n    helloWorld\n  }\n": types.MessageSubDocument,
-};
+  '\n    subscription messages {\n        watchMessages\n    }\n':
+    types.MessagesDocument,
+  '\n    mutation sendMessage($message: String!) {\n        sendMessage(message: $message)\n    }\n':
+    types.SendMessageDocument,
+  '\nquery getHero {\n  hero {\n    name\n  }\n}\n': types.GetHeroDocument,
+  '\n  query notHero {\n    notHero {\n      name\n    }\n  }\n':
+    types.NotHeroDocument,
+  '\n  subscription MessageSub {\n    helloWorld\n  }\n':
+    types.MessageSubDocument
+}
 
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
@@ -32,31 +36,42 @@ const documents = {
  * The query argument is unknown!
  * Please regenerate the types.
  */
-export function graphql(source: string): unknown;
+export function graphql(source: string): unknown
 
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n    subscription messages {\n        watchMessages\n    }\n"): (typeof documents)["\n    subscription messages {\n        watchMessages\n    }\n"];
+export function graphql(
+  source: '\n    subscription messages {\n        watchMessages\n    }\n'
+): (typeof documents)['\n    subscription messages {\n        watchMessages\n    }\n']
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n    mutation sendMessage($message: String!) {\n        sendMessage(message: $message)\n    }\n"): (typeof documents)["\n    mutation sendMessage($message: String!) {\n        sendMessage(message: $message)\n    }\n"];
+export function graphql(
+  source: '\n    mutation sendMessage($message: String!) {\n        sendMessage(message: $message)\n    }\n'
+): (typeof documents)['\n    mutation sendMessage($message: String!) {\n        sendMessage(message: $message)\n    }\n']
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\nquery getHero {\n  hero {\n    name\n  }\n}\n"): (typeof documents)["\nquery getHero {\n  hero {\n    name\n  }\n}\n"];
+export function graphql(
+  source: '\nquery getHero {\n  hero {\n    name\n  }\n}\n'
+): (typeof documents)['\nquery getHero {\n  hero {\n    name\n  }\n}\n']
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  query notHero {\n    notHero {\n      name\n    }\n  }\n"): (typeof documents)["\n  query notHero {\n    notHero {\n      name\n    }\n  }\n"];
+export function graphql(
+  source: '\n  query notHero {\n    notHero {\n      name\n    }\n  }\n'
+): (typeof documents)['\n  query notHero {\n    notHero {\n      name\n    }\n  }\n']
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  subscription MessageSub {\n    helloWorld\n  }\n"): (typeof documents)["\n  subscription MessageSub {\n    helloWorld\n  }\n"];
+export function graphql(
+  source: '\n  subscription MessageSub {\n    helloWorld\n  }\n'
+): (typeof documents)['\n  subscription MessageSub {\n    helloWorld\n  }\n']
 
 export function graphql(source: string) {
-  return (documents as any)[source] ?? {};
+  return (documents as any)[source] ?? {}
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
+  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never
